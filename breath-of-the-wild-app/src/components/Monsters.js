740a07ebb0a9e776511e2f556bf6c6fb44371ba3
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-07-01 17:32:19 -0700

    Standardize export default and add image support

--- breath-of-the-wild-app/src/components/Monsters.js
+++ breath-of-the-wild-app/src/components/Monsters.js
@@ -1,11 +1,21 @@
-export default function Monsters({ data }) {
-    return (
-      <div>
-        <p>Monsters</p>
-        {data && data.length > 0 ? data.map((monster, i) => (
-          <p key={i}>{monster.name}</p>
-        )) : <p>No monsters found</p>}
-      </div>
-    );
-  }
-  
+import React from "react";
+
+function Monsters({ data }) {
+  return (
+    <div>
+      <p>Monsters</p>
+      {data && data.length > 0 ? (
+        data.map((monster, i) => (
+          <div key={i}>
+            <img src={monster.image} alt={monster.name} />
+            <p>{monster.name}</p>
+          </div>
+        ))
+      ) : (
+        <p>No monsters found</p>
+      )}
+    </div>
+  );
+}
+
+export default Monsters;
