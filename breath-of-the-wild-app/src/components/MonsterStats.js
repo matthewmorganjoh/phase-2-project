9ec6e471a32a0402dc59c42b51d5f74862098c93
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-07-02 11:30:16 -0700

    Replace div with containers for centering

--- breath-of-the-wild-app/src/components/MonsterStats.js
+++ breath-of-the-wild-app/src/components/MonsterStats.js
@@ -1,24 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+const Container=``;
+const Image=``;
+const Heading=``;
+const Paragraph=``;
+
+
 function MonsterStats({ data }) {
   const { id } = useParams();
 
   const monster = data.find((monster) => monster.id.toString() === id);
 
   return (
-    <div>
+    <Container>
       {monster ? (
-        <div>
-          <h2>{monster.name}</h2>
-          <img src={monster.image} alt={monster.name} />
-          <p>Description: {monster.description}</p>
-          <p>Common Locations: {monster.common_locations && monster.common_locations.length > 1 ? monster.common_locations.join(", ") : monster.common_locations}</p>
-        </div>
+        <>
+          <Heading>{monster.name}</Heading>
+          <Image src={monster.image} alt={monster.name} />
+          <Paragraph>Description: {monster.description}</Paragraph>
+          <Paragraph>
+            Common Locations:{" "}
+            {monster.common_locations &&
+            monster.common_locations.length > 1
+              ? monster.common_locations.join(", ")
+              : monster.common_locations}
+          </Paragraph>
+        </>
       ) : (
         <p>Monster not found</p>
       )}
-    </div>
+    </Container>
   );
 }
 
